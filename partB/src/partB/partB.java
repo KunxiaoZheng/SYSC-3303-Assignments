5ce17f20f1a166d357d99f9d09939ad45eb8519f
KunxiaoZheng <jimmybacktotheroad@hotmail.com>
2014-01-24 17:22:57 -0500

    add the numOfThreads to control the number of threads

--- partB/src/partB/partB.java
+++ partB/src/partB/partB.java
@@ -4,18 +4,19 @@ public class partB {
 	public static long number;
 	public static void main(String [] args)throws InterruptedException{
 		number=0;
+		int numOfThreads=5;
 		addCounter add=new addCounter();
 		subCounter sub=new subCounter();
-		Thread[] addThreads=new Thread[5];
-		Thread[] subThreads=new Thread[5];
+		Thread[] addThreads=new Thread[numOfThreads];
+		Thread[] subThreads=new Thread[numOfThreads];
 		
-		for(int i =0; i<5;i++){
+		for(int i =0; i<numOfThreads;i++){
 			addThreads[i]=new Thread(add);
 			subThreads[i]=new Thread(sub);
 			addThreads[i].start();
 			subThreads[i].start();
 		}
-		for(int i =0; i<5;i++){
+		for(int i =0; i<numOfThreads;i++){
 			addThreads[i].join();
 			subThreads[i].join();
 		}
